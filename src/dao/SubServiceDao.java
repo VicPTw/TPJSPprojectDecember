c00d0c74f3e397f3fdb48d8bb6d26ce59fa16cd8
VicPeng <45628284+VicPTw@users.noreply.github.com>
2018-12-07 16:43:14 +0800

    new method
    getAllRecordsByRef(String sId)

--- src/dao/SubServiceDao.java
+++ src/dao/SubServiceDao.java
@@ -2,10 +2,12 @@
 package dao;
 
 import bean.SubService;
+import java.util.List;
 
 /**
  *
  * @author VicP
  */
 public interface SubServiceDao extends IDao<SubService> {
+    List<SubService> getAllRecordsByRef(String sId);
 }
